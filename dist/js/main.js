114ddc4076f2cd8eaf5e04e4d4f4636908d828b2
VichaPratummaneeTHA <66987801+VichaPratummaneeTHA@users.noreply.github.com>
2020-10-04 14:39:01 +0700

    Initial Commit

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+// Get the Current years for the copyright
+
+$("#year").text(new Date().getFullYear());
+
+// Init Scrollsay
+
+$("body").scrollspy({ target: "#main-nav" });
+
+// Smooth Scrolling
+
+$("#main-nav a").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      500,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
+});
